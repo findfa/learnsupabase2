58baaf1fc26742cfc32168ee22711635dfbeb568
Seth Iradukunda <sethiradukunda113@gmail.com>
2025-08-26 03:18:59 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const SUPABASE_URL = "https://YOUR_PROJECT_URL.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
-
+const SUPABASE_URL = "https://gmfgchuizwemqcafdtcd.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtZmdjaHVpendlbXFjYWZkdGNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxOTg5MDIsImV4cCI6MjA3MTc3NDkwMn0.uchcIzuwXntPCxLPlOQQ7YgEyTd5xI7lAHt33F365C4";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
